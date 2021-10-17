195e24f159a8318180f53bc59aa3e1dd920d0b5f
vivekduttamishra <vivek@conceptarchitect.in>
2021-10-17 17:50:16 +0530

    day#4 update#3

---
+++ Programs/WPFDemos/WPF04Layouts/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF04Layouts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowStackPanelDemo(object sender, RoutedEventArgs e)
+        {
+            var win = new StackPanelDemo();
+            win.Show();
+
+        }
+
+        private void ShowUniformGridDemo(object sender, RoutedEventArgs e)
+        {
+            var win = new UniformGridDemo();
+            win.Show();
+        }
+
+        private void ShowDockPanelDemo(object sender, RoutedEventArgs e)
+        {
+            var win = new DockPanelDemo();
+            win.Show();
+        }
+    }
+}
