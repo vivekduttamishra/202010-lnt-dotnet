5a7bf3fa1a708220bb40267a17e59729cc2f4a6c
vivekduttamishra <vivek@conceptarchitect.in>
2021-10-21 17:31:14 +0530

    day#5 udpate#4

---
+++ Programs/WPFDemos/ConceptArchitect.Wpf/Date.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace ConceptArchitect.Wpf
+{
+    public class Date : MarkupExtension
+    {
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ToString();  //return this value to whoever ask.
+        }
+
+
+
+        public DateTime date;
+
+        public string Value
+        {
+            get
+            {
+                return date.ToString();
+            }
+            set
+            {
+                date = DateTime.Parse(value);
+            }
+        }
+
+        
+        public string Format { get; set; }
+
+        public Date()
+        {
+            date = DateTime.Now;
+            Format = "dd-MMM-yyyy, HH:mm:ss";
+        }
+        public Date(string value)
+        {
+            Value = value;
+        }
+
+
+        public override string ToString()
+        {
+            return date.ToString(Format);
+        }
+
+       
+    }
+}
