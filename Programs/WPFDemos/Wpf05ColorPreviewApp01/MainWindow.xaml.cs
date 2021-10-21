ace1679302768ef2cfc411d6e2b58583d4a621f4
vivekduttamishra <vivek@conceptarchitect.in>
2021-10-21 15:01:25 +0530

    day#5 udpate#3

--- Programs/WPFDemos/Wpf05ColorPreviewApp01/MainWindow.xaml.cs
+++ Programs/WPFDemos/Wpf05ColorPreviewApp01/MainWindow.xaml.cs
@@ -25,11 +25,49 @@ namespace Wpf05ColorPreviewApp01
             InitializeComponent();
         }
 
-        private void UpdateColor(object sender, RoutedEventArgs e)
-        {
-            //preview.Background = "Red";
 
-            preview.Background = Brushes.Red;  
+
+        private void UpdatePreview()
+        {
+            //previewBox.Background = new SolidColorBrush(Color.FromRgb(251, 151, 51));
+
+            previewBox.Background = GetNamedColor(colorNameTextBox.Text);
+
+        }
+
+        private Brush GetNamedColor(string text)
+        {
+            Brush defaultBrush = Brushes.Transparent;//Brushes.White;
+            if (string.IsNullOrEmpty(text))
+                return defaultBrush;
+
+            switch (text.ToLower())
+            {
+                case "red": return Brushes.Red;
+                case "green": return Brushes.Green;
+                case "blue":return Brushes.Blue;
+                case "black": return Brushes.Black;
+                case "white": return Brushes.White;
+            }
+
+            return defaultBrush;
+        }
+
+        private void HandleUpdatePreview(object sender, RoutedEventArgs e)
+        {
+            UpdatePreview();
+        }
+
+        private void OnCheckChange(object sender, RoutedEventArgs e)
+        {
+            previewButton.IsEnabled = !autoPreviewCheckBox.IsChecked.Value;  //Is Checked is nullble boolean. Can have value true/false/null
+            UpdatePreview();
+        }
+
+        private void OnColorNameChange(object sender, TextChangedEventArgs e)
+        {
+            if (autoPreviewCheckBox.IsChecked.Value)
+                UpdatePreview();
         }
     }
 }
