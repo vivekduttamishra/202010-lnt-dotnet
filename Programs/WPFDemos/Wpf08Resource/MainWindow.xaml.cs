c7101741cb46a8f4e01850f373b80f92735aa21f
vivekduttamishra <vivek@conceptarchitect.in>
2021-10-22 10:59:32 +0530

    day#6 udpate#1

---
+++ Programs/WPFDemos/Wpf08Resource/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ConceptArchitect.BooksApi;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+
+
+
+
+
+namespace Wpf08Resource
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			book1Button.Resources["book"] = new Book
+			{
+				Title = "The Accursed God",
+				Author = "Vivek Dutta Mishra",
+				Price = 399
+			}; 
+
+			book1Button.Resources["VisitedColor"] = Brushes.LightCoral;
+		}
+
+		
+		private void ShowBook(object sender, RoutedEventArgs e)
+		{
+			var button = sender as Button;
+
+			var book = button.Resources["book"] as Book;
+
+			if (book != null)
+			{
+				MessageBox.Show(book.ToString(), "About Book", MessageBoxButton.OK, MessageBoxImage.Information);
+				var brush= button.Resources["VisitedColor"] as Brush;
+				if (brush!=null)					
+					button.Background = brush;
+
+			}
+			else
+            {
+				MessageBox.Show("No Book Associated with this button", "Not Found", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+	}
+}
